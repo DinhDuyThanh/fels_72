50cdcd239b1d8304d3fe0ce9669dc137097e2e53
BichVN <tieuke107@gmail.com>
2015-07-30 11:26:50 +0700

    add relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,16 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length:{minimum: 6}
 
   has_secure_password
+
+  def follow other_user 
+    active_relationships.create followed_id: other_user.id
+  end
+
+  def unfollow other_user
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following? other_user
+    following.include? other_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
 
-  resources :users
   resources :categories
+  resources :users 
+  resources :relationships, only: [:create, :destroy]
 end
-
