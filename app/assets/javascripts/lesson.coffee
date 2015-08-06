ce9140f4473ee27653c0aaaf2ace88c7c29625f3
Thanh <dinhduythanh51292@gmail.com>
2015-08-06 15:29:36 +0700

    firt commit

---
+++ app/assets/javascripts/lesson.coffee
@@ -0,0 +1,5 @@
+jQuery(document).on("ready page:load", function() {
+  $(function () { 
+    alert("ok");
+  });
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ end
 categories = Category.order(:created_at).take(5)
 10.times do
   name = Faker::Lorem.sentence(5)
+  description = Faker::Lorem.sentence(5)
   categories.each {|category| category.lessons.create! name: name}
 end
 
